1c629dcafc4f833a112446e69214ad5d8f8d6d36
Zekarant <vegeta42660@gmail.com>
2022-01-03 16:42:31 +0100

    Mise à jour V2 portfolio

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Portfolio Développeur Web - CHOMEL Colin</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="css/accueil.css">
+</head>
+<body>
+	<?php include('components/navbar.php'); ?>
+	<div id="entete">
+		<h1 class="mainTitle">Colin</h1>
+		<h4 class="secondaryTitle">Portfolio</h4>
+		<p class="introduction">Bienvenue sur le portfolio d'un jeune développeur passionné par le web. J'espère que mon univers vous plaira !</p>
+	</div>
+	<hr class="separation">
+	<h2 class="aboutTitle text-center">A propos</h2>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 align-self-center">
+				<p>Je suis CHOMEL Colin, actuellement étudiant en Licence Professionnelle ATII (Animateur des Technologies de l'Animation et de l'Internet) à l'IUT de Saint-Etienne (Loire - 42). Je me suis orienté sur cette formation parce que je suis passionné par le monde du développement web et je souhaitais donc accroitre mes connaissances.</p>
+				<p>Vous pourrez retrouver sur ce portfolio mes compétences, une partie de mon parcours scolaire ainsi que mes différents projets.</p>
+				<p>Vous avez également la possibilité de me contacter grâce au lien "Contact" situé en bas de page.</p>
+			</div>
+			<div class="col-lg-6 align-self-center imageAbout">
+			</div>
+		</div>
+	</div>
+	<hr class="separation2">
+</body>
+</html>
